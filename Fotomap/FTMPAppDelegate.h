208128ceb544e619dfd98c72b12e4d9fb26b5e58
Matt Quiros <mattquiros@gmail.com>
2014-04-21 17:13:49 +0800

    Initial commit

---
+++ Fotomap/FTMPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FTMPAppDelegate.h
+//  Fotomap
+//
+//  Created by Matt Quiros on 4/21/14.
+//  Copyright (c) 2014 Action Stack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTMPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Fotomap/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Fotomap
+//
+//  Created by Matt Quiros on 4/21/14.
+//  Copyright (c) 2014 Action Stack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FTMPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FTMPAppDelegate class]));
+    }
+}
